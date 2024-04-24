241d93e2bfe07a308af43014cd149ad4fa57e3e5
fffff1 <yanasadovnikova68@gmail.con>
2024-04-24 10:48:09 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("A >> ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("B >> ");
+                int b = int.Parse(Console.ReadLine());
+
+                Console.Write("Действие >> ");
+                string c = Console.ReadLine();
+
+                switch (c)
+                {
+                    case "*":
+                        Console.WriteLine(a * b);
+                        break;
+                    case "/":
+                        Console.WriteLine(a / b);
+                        break;
+                    case "-":
+                        Console.WriteLine(a - b);
+                        break;
+                    case "+":
+                        Console.WriteLine(a + b);
+                        break;
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
